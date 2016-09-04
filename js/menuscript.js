74a2c2d887acde06a58ee255c7998507492a0d2a
Shadab Khan <shadabk96@gmail.com>
2016-09-04 19:04:17 +0530

    adding to menu

--- js/menuscript.js
+++ js/menuscript.js
@@ -1,11 +1,16 @@
+//function Estabilish connection to database
+let connection;
+estcon();
 //Estabilish connection to database
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'resto-man-db'
-});
+function estcon(){
+  var mysql      = require('mysql');
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'resto-man-db'
+  });
+}
 
 // estabilises connection from sql
 connection.connect(function(err){
@@ -23,12 +28,15 @@ connection.query('SELECT * FROM menu',function(err,rows){
   console.log('Data received from Db:\n');
   console.log(rows);
 // Print row to table
-  var json = rows;
-  for (var i = 0; i < json.length; i++) {
+  printtable(rows);
+});
+
+function printtable(json){
+  for (var i = json.length-1 ; i >= 0; i--) {
     // Find a <table> element with id="myTable":
     var table = document.getElementById("myTable");
     // Create an empty <tr> element and add it to the 1st position of the table:
-    var row = table.insertRow(0);
+    var row = table.insertRow(1);
     // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
@@ -38,7 +46,31 @@ connection.query('SELECT * FROM menu',function(err,rows){
     cell2.innerHTML = json[i].name;
     cell3.innerHTML = json[i].price;
   }
-});
+}
+
+// Add a new item to menu
+function addtomenu(){
+  //Estabilish connection
+  estcon();
+  // get values from form
+  var vitem_code = document.getElementById("item_code").value;
+  var vname = document.getElementById("name").value;
+  var vprice = document.getElementById("price").value;
+  var itementry = { item_code: vitem_code, name: vname, price: vprice };
+  var q = connection.query('INSERT INTO menu SET ?', itementry, function(err,res){
+  if(err) throw err;
+    console.log('Last insert ID:', res.insertId);
+    connection.query('SELECT * FROM menu',function(err,rows){
+      if(err) throw err;
+    // Fetch to console
+      console.log('Data received from Db:\n');
+      console.log(rows);
+    // Print row to table
+      printtable(rows);
+    });
+  });
+  return;
+}
 
 // terminates connection
 connection.end();
