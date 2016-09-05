4484d8a6e0ad961402adddc7d29023993ad50490
Shadab Khan <shadabk96@gmail.com>
2016-09-05 11:55:21 +0530

    menucard complete

--- js/menuscript.js
+++ js/menuscript.js
@@ -1,7 +1,7 @@
-//function Estabilish connection to database
+//Estabilish connection to database
 let connection;
 estcon();
-//Estabilish connection to database
+//function to Estabilish connection to database
 function estcon(){
   var mysql      = require('mysql');
   connection = mysql.createConnection({
@@ -21,31 +21,51 @@ connection.connect(function(err){
   console.log('Connection established');
 });
 
-// calls query
-connection.query('SELECT * FROM menu',function(err,rows){
-  if(err) throw err;
-// Fetch to console
-  console.log('Data received from Db:\n');
-  console.log(rows);
-// Print row to table
-  printtable(rows);
-});
+printtable();
 
-function printtable(json){
-  for (var i = json.length-1 ; i >= 0; i--) {
-    // Find a <table> element with id="myTable":
-    var table = document.getElementById("myTable");
-    // Create an empty <tr> element and add it to the 1st position of the table:
-    var row = table.insertRow(1);
-    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    // Add some text to the new cells:
-    cell1.innerHTML = json[i].item_code;
-    cell2.innerHTML = json[i].name;
-    cell3.innerHTML = json[i].price;
-  }
+function printtable(){
+  // calls query
+  connection.query('SELECT * FROM menu',function(err,rows){
+    if(err) throw err;
+  // Fetch to console
+    console.log('Data received from Db:\n');
+    console.log(rows);
+    var json = rows;
+    for (var i = json.length-1 ; i >= 0; i--) {
+      // Find a <table> element with id="myTable":
+      var table = document.getElementById("myTable");
+      // Create an empty <tr> element and add it to the 1st position of the table:
+      var row = table.insertRow(1);
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      // Add some text to the new cells:
+      cell1.innerHTML = json[i].item_code;
+      cell2.innerHTML = json[i].name;
+      cell3.innerHTML = json[i].price;
+      cell4.innerHTML = json[i].item_code;
+      cell4.onclick = function(){delMenuItem(this.innerHTML)};
+    }
+    return;
+  });
+}
+
+// delete item from database
+function delMenuItem(index){
+  estcon();
+  console.log(index + 'Deleted');
+  connection.query(
+    'DELETE FROM menu WHERE item_code = ?',
+    index,
+    function (err, result) {
+      if (err) throw err;
+      console.log('Deleted ' + result.affectedRows + ' rows');
+    }
+  );
+window.location.reload();
+  return;
 }
 
 // Add a new item to menu
@@ -60,14 +80,7 @@ function addtomenu(){
   var q = connection.query('INSERT INTO menu SET ?', itementry, function(err,res){
   if(err) throw err;
     console.log('Last insert ID:', res.insertId);
-    connection.query('SELECT * FROM menu',function(err,rows){
-      if(err) throw err;
-    // Fetch to console
-      console.log('Data received from Db:\n');
-      console.log(rows);
-    // Print row to table
-      printtable(rows);
-    });
+    printtable();
   });
   return;
 }
